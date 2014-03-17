d053f9fa574079ab8ef0913a394ef34a39c05143
John Wu <webmaster@leapoahead.com>
2014-03-17 09:36:38 +0800

    bug fix

--- js/main.js
+++ js/main.js
@@ -2,15 +2,31 @@ var taobao = angular.module('taobao', []);
 
 taobao.run(function($templateCache) {
   $templateCache.put('drpd', '<div class="drpd" ng-show="isVisible" ng-transclude> </div>');
-  $templateCache.put('tb-btn', '<button class="taobao-button" ng-transclude> </button>')
+  $templateCache.put('tbBtn', '<button class="taobao-button" ng-transclude> </button>');
+  $templateCache.put('cartCount', '<strong class="tb-red">{{ cartCount }}</strong>');
 });
 
+// 伪API调用
 taobao.service('tbAPI', function() {
   this.getCartCount = function() {
-    return 1;
+    return 0;
   }
 });
 
+// 购物车数量 显示组件
+taobao.directive('cartCount', function($templateCache, tbAPI) {
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: true,
+    template: $templateCache.get('cartCount'),
+    link: function(scope, el, attrs) {
+      scope.cartCount = tbAPI.getCartCount();
+    }
+  }
+});
+
+// 下拉菜单
 taobao.directive('drpd', function($templateCache) {
   return {
     restrict: 'AE', 
@@ -32,13 +48,14 @@ taobao.directive('drpd', function($templateCache) {
   }
 });
 
+// 按钮
 taobao.directive('tbBtn', function($templateCache) {
   return {
-    restrict: 'E',
+    restrict: 'EA',
     transclude: true,
     replace: true,
     scope: true,
-    template: $templateCache.get('tb-btn'),
+    template: $templateCache.get('tbBtn'),
     link: function(scope, el, attrs) {
       $(el).addClass('taobao-button-' + attrs.tbBtnType);
     }
